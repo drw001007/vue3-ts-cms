12f3fdb9a6b441438147b4aeff2540fbad41eead
drw001007 <1793998240@qq.com>
2022-07-08 18:38:12 +0800

    feat(uicomponents): import element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <h2>{{ $store.state.name }}</h2>
-  <router-link to="/login">登录</router-link>
-  <router-link to="/main">首页</router-link>
-  <router-view></router-view>
+  <div>
+    <h2>{{ $store.state.name }}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
+
+    <el-button>按钮</el-button>
+  </div>
 </template>
 
 <script lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+
+// 全局引用
+// import ElementPlus from 'element-plus'
+// import 'element-plus/theme-chalk/index.css'
+
 import App from './App.vue'
 
 import router from './router/index'
@@ -7,4 +12,5 @@ import store from './store/index'
 const app = createApp(App)
 app.use(router)
 app.use(store)
+// app.use(ElementPlus)
 app.mount('#app')
